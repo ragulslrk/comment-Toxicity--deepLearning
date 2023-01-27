5e69d7b3c5d72626ed25a535dda3c3bcb6d1f06a
ragulslrk <slrk4444@gmail.com>
2023-01-27 23:01:58 +0530

    final version

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 import tensorflow as tf 
 from  tensorflow.keras.layers import TextVectorization
 import  numpy as np
+from flask_cors import CORS
+CORS(app)
 
 #ml  vector  loading
 from_disk=pickle.load( open('models/vector_layer.pkl','rb') )
@@ -20,24 +22,33 @@ model=tf.keras.models.load_model('models/toxicity.h5')
 
 @app.route('/')
 def index():
-    return jsonify({"USER": "This is  backend server for comments toxicity prediction using deep learning "})
+    print('in index')
+    resp=jsonify({"USER": "This is  backend server for comments toxicity prediction using deep learning "})
+    resp.headers.add("Access-Control-Allow-Origin", "*")
+    return  resp
 
 
 # route  for prediction
-@app.route('/predict',methods=['POST'])
-def predict():
-    if request.method=="POST":
-        comments=request.form['comments']
-        input_str=new_vectorizer(comments)
-        # print(input_str)
-        res=model.predict(np.expand_dims(input_str,0))
-        # print(res)
-        return jsonify({"toxic":str(res[0][0]),
+@app.route('/predict/<comments>')
+def predict(comments):
+    print(comments)
+    comments=str(comments)
+    input_str=new_vectorizer(comments)
+    print(input_str)
+    res=model.predict(np.expand_dims(input_str,0))
+    print(res)
+    return jsonify({"toxic":str(res[0][0]),
                         "severe_toxic":str(res[0][1]),
                         "obscene":str(res[0][2]),
                         "threat":str(res[0][3]),
                         "insult":str(res[0][4]),
                         "identity_hate":str(res[0][5])})
+
+
+# post  method way
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
